1260e0278430aecaa409321b46c42147ac1c25a5
Nicholas Cheng <nicholas@mail.puncaktegap.com.my>
2026-02-01 10:12:16 +0800

    update

---
+++ event_data.js
@@ -0,0 +1,22 @@
+const eventData = {
+    "activeQuests": [
+        {
+            "date": "2 February 2026",
+            "title": "Carefree BG Meetups",
+            "details": "Come play some good ol BGs with us on Thaipusam! Bring your partner or find a teammate! New players welcome!",
+            "location": "Union Coffee, Citta Mall, Ara Damansara",
+            "time": "1:30 PM - 7:00 PM",
+            "mapUrl": "https://maps.app.goo.gl/hPKRMf2bwCkV3NWq6"
+        }
+    ],
+    "completedQuests": [
+        {
+            "date": "31 January 2026",
+            "title": "Fabulous BG Saturdays",
+            "details": "Bring your partner or find a teammate! New players welcome!",
+			"location": "Psyclist Cafe, Dataran PHB, Saujana Resort, Shah Alam",
+            "time": "1:30 PM - 7:00 PM",
+            "mapUrl": "https://maps.app.goo.gl/BLNhMudEfUExwwit5"
+        }
+    ]
+};
