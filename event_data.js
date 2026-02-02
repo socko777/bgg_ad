118b0b5d151fa27bb2e58011af593061aee989ef
Nicholas Cheng <nicholas@mail.puncaktegap.com.my>
2026-02-02 09:33:31 +0800

    update

--- event_data.js
+++ event_data.js
@@ -1,22 +1,26 @@
-const eventData = {
-    "activeQuests": [
-        {
-            "date": "2 February 2026",
-            "title": "Carefree BG Meetups",
-            "details": "Come play some good ol BGs with us on Thaipusam! Bring your partner or find a teammate! New players welcome!",
-            "location": "Union Coffee, Citta Mall, Ara Damansara",
-            "time": "1:30 PM - 7:00 PM",
-            "mapUrl": "https://maps.app.goo.gl/hPKRMf2bwCkV3NWq6"
-        }
-    ],
-    "completedQuests": [
-        {
-            "date": "31 January 2026",
-            "title": "Fabulous BG Saturdays",
-            "details": "Bring your partner or find a teammate! New players welcome!",
-			"location": "Psyclist Cafe, Dataran PHB, Saujana Resort, Shah Alam",
-            "time": "1:30 PM - 7:00 PM",
-            "mapUrl": "https://maps.app.goo.gl/BLNhMudEfUExwwit5"
-        }
-    ]
-};
+const eventData = [
+    {
+        "date": "7 February 2026",
+        "title": "Fabulous BG Saturdays",
+        "details": "Come play at our weekly Saturday sessions! Everyone is welcome!",
+        "location": "Psyclist Cafe, Dataran PHB, Saujana Resort, Shah Alam",
+        "time": "1:30 PM - 7:00 PM",
+        "mapUrl": "https://maps.app.goo.gl/BLNhMudEfUExwwit5"
+    },
+    {
+        "date": "2 February 2026",
+        "title": "Carefree BG Meetups",
+        "details": "Welcome to join our impromptu BG sessions, just remember to bring the smiles!",
+        "location": "Union Coffee, Citta Mall, Ara Damansara",
+        "time": "1:30 PM - 7:00 PM",
+        "mapUrl": "https://maps.app.goo.gl/hPKRMf2bwCkV3NWq6"
+    },
+    {
+        "date": "31 January 2026",
+        "title": "Fabulous BG Saturdays",
+        "details": "Come play at our weekly Saturday sessions! Everyone is welcome!",
+        "location": "Psyclist Cafe, Dataran PHB, Saujana Resort, Shah Alam",
+        "time": "1:30 PM - 7:00 PM",
+        "mapUrl": "https://maps.app.goo.gl/BLNhMudEfUExwwit5"
+    }
+];
