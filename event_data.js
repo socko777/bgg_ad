4fdca88c64c6cd667736a510b3e130da78d299e1
Nicholas Cheng <nicholas@mail.puncaktegap.com.my>
2026-02-09 14:34:07 +0800

    update

--- event_data.js
+++ event_data.js
@@ -20,6 +20,13 @@ const eventData = [
         "details": "Join us for a slow Saturday in the Garden. A thoughtfully curated afternoon of strategy, connection, and mindful play. Whether you’re a seasoned strategist or holding your first deck, we’ve kept a seat warm for you. Come for the games, stay for the company and the serene atmosphere.",
         "location": "Union Coffee, Citta Mall, Ara Damansara",
         "time": "1:30 PM - 7:00 PM",
-        "mapUrl": "https://maps.app.goo.gl/BLNhMudEfUExwwit5"
+        "mapUrl": "https://maps.app.goo.gl/hPKRMf2bwCkV3NWq6"
+    },{
+        "date": "8 February 2026",
+        "title": "Carefree BG Meetups",
+        "details": "Unwind with us for a lighthearted afternoon of carefree play. We’ve traded deep complexity for cozy discovery and easy laughter. Whether you're here to learn something new or simply enjoy the company, pull up a chair and let the week melt away in our garden sanctuary.",
+        "location": "Union Coffee, Citta Mall, Ara Damansara",
+        "time": "1:30 PM - 7:00 PM",
+        "mapUrl": "https://maps.app.goo.gl/hPKRMf2bwCkV3NWq6"
     },
 ];
