c65b51738d3d78b2e1e1e48b9eb616a55abeb3e9
Nicholas Cheng <nicholas@mail.puncaktegap.com.my>
2026-02-09 11:12:14 +0800

    update

--- event_data.js
+++ event_data.js
@@ -14,5 +14,12 @@ const eventData = [
         "location": "Union Coffee, Citta Mall, Ara Damansara",
         "time": "1:30 PM - 7:00 PM",
         "mapUrl": "https://maps.app.goo.gl/hPKRMf2bwCkV3NWq6"
-    }
+    }, {
+        "date": "14 February 2026",
+        "title": "Fabulous BG Saturdays",
+        "details": "Join us for a slow Saturday in the Garden. A thoughtfully curated afternoon of strategy, connection, and mindful play. Whether you’re a seasoned strategist or holding your first deck, we’ve kept a seat warm for you. Come for the games, stay for the company and the serene atmosphere.",
+        "location": "Psyclist Cafe, Dataran PHB, Saujana Resort, Shah Alam",
+        "time": "1:30 PM - 7:00 PM",
+        "mapUrl": "https://maps.app.goo.gl/BLNhMudEfUExwwit5"
+    },
 ];
